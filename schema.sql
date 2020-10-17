fac3dbe15f74445d0d5d432c3ff5f4b112ee2ced
Роман <roman.buster@mail.ru>
2020-10-17 12:06:35 +0500

    add config.ini and setup.py

--- schema.sql
+++ schema.sql
@@ -6,3 +6,26 @@ CREATE TABLE users (
         is_today BOOLEAN
 )
 CREATE TABLE organizations(
+id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+organizations TEXT,
+faculty TEXT,
+studGroup TEXT,
+tag TEXT
+);
+CREATE TABLE schedule(
+id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+tag TEXT, day TEXT,
+number INTEGER,
+type TEXT,
+startTime TEXT,
+endTime,TEXT
+title TEXT,
+classroom TEXT,
+lecturer
+);
+CREATE TABLE reports(
+report_id INTEGER PRIMARY KEY AUTOINCREMENT,
+user_id INTEGER,
+report TEXT,
+date DATETIME
+);
